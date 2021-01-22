757f84c48d001a626f1bca0118d38bb224b3ebb9
Pallab Mahapatra <pallabmahapatra@gmail.com>
2021-01-22 16:26:22 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,4 +1,20 @@
-import numpy as np
+import unittest
+from requests.exceptions import Timeout
+from unittest.mock import Mock
 
-# written in second branch
-import seaborn as sns
+requests=Mock()
+
+def get_holidays():
+   r=requests.get("httpL://localhost/api/holidays")
+   if r.status_code==200:
+       return r.json()
+   return None
+
+class TestCalender(unittest.TestCase):
+    def test_get_holidays_timeout(self):
+        requests.get.side_effect=Timeout
+        with self.assertRaises(Timeout):
+            get_holidays()
+
+if __name__=='__main__':
+    unittest.main()
